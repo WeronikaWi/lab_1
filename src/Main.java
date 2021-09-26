5481f405c5d13b95258e0d7a04d46f6d0f378ae5
weron <weronikawiecaszek@wp.pl>
2021-09-26 09:08:32 +0200

    ćwiczenie pierwsze

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
         System.out.println("Nie wiedziała jak." );
         System.out.printf( "A %s wiedział, nie %s \n", "dziad", "powiedział");
         System.out.printf("Napisałam %d linie tekstu i nie pamiętam co było dalej", 4);
+        System.out.println("Nie ");
 
     }
 }
