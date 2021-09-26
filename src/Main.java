8199e42ad085ae831359b115c73edfa945c40fb2
weron <weronikawiecaszek@wp.pl>
2021-09-26 10:18:42 +0200

    ćwiczenie czwarte

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
         System.out.println("!(a && b) || (a && !c) to: " + (!(a && b) || (a && !c))); //true
         System.out.println(" x > y || z <= z to " +(x > y || z <= z)); //true
         System.out.println(" x == z && z != y to " +(x == z && z != y)); //false
-        System.out.println(" x + z == y && x + y != z to " + (x + z == y && x + y != z));
+        System.out.println(" x + z == y && x + y != z to " + (x + z == y && x + y != z)); //true
         System.out.println("!(a && b) == (!a || !b) to " + (!(a && b) == (!a || !b))); //true
         System.out.println("!(a && b) == (!a && !b) to " + (!(a && b) == (!a || b))); //false
 
