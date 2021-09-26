27df77e84e385ac831a80c5d39417f3d660730ca
weron <weronikawiecaszek@wp.pl>
2021-09-26 10:48:13 +0200

    ćwiczenie czwarte

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,33 @@ import java.util.Scanner;
 
 public class Main {
 
+
     public static void main(String[] args) {
 
         /* ćw.4 */
 
         /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
 
-        boolean a = true, b = false, c = true;
-        int x = 5, y = 23, z = 18;
-        System.out.println("!(a && b) || (a && !c) to: " + (!(a && b) || (a && !c))); //true
-        System.out.println(" x > y || z <= z to " +(x > y || z <= z)); //true
-        System.out.println(" x == z && z != y to " +(x == z && z != y)); //false
-        System.out.println(" x + z == y && x + y != z to " + (x + z == y && x + y != z)); //true
-        System.out.println("!(a && b) == (!a || !b) to " + (!(a && b) == (!a || !b))); //true
-        System.out.println("!(a && b) == (!a && !b) to " + (!(a && b) == (!a || b))); //false
+        int[] tab = {5, 8, 1, 8, 1, 8, 9, 10, -45};
 
+        for (int liczba : tab) {
+            System.out.print(liczba + " ");
+
+        }
+        System.out.println();
+        for (int i = 0; i < tab.length; i++) {
+            for (int j = 0; j < tab.length - i - 1; j++) {
+                if (tab[j] > tab[j + 1]) {
+                    int temp = tab[j];
+                    tab[j] = tab[j + 1];
+                    tab[j + 1] = temp;
+                }
+            }
+        }
+        for (int liczba : tab) {
+            System.out.print(liczba + " ");
+
+        }
 
 
     }
