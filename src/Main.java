74be7d20cf8f277cc2ef1c75a8fac58e09a310f1
weron <weronikawiecaszek@wp.pl>
2021-09-26 08:35:51 +0200

    ćwiczenie pierwsze

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ public class Main {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
 
+        System.out.print( "Siała baba mak. ");
+        System.out.println("Nie wiedziała jak." );
+        System.out.printf( "A %s wiedział, nie %s \n", "dziad", "powiedział");
+        System.out.printf("Napisałam %d linie tekstu i nie pamiętam co było dalej", 4);
 
     }
 }
