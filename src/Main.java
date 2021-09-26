54db5546be05359c6134a7c22da8a4a287efea5a
weron <weronikawiecaszek@wp.pl>
2021-09-26 10:14:35 +0200

    ćwiczenie szóste

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,17 @@ public class Main {
 
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
+        Scanner scan = new Scanner(System.in);
+        double[] tab = new double[5];
+        for (int i = 0; i < tab.length; i++)
+        {
+            System.out.printf("Podaj %d-ty wyraz tablicy: ", i+1);
+            tab[i] = scan.nextDouble();
+        }
+        System.out.print("Zawartośc tablicy: ");
+        for (double liczba: tab) {
+            System.out.print(liczba + " ");
+        }
 
     }
 }
