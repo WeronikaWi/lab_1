d20f4c1d3d0f919dfe46e9396ee424b560ac4607
weron <weronikawiecaszek@wp.pl>
2021-09-26 09:23:31 +0200

    ćwiczenie trzecie

--- src/Main.java
+++ src/Main.java
@@ -12,10 +12,33 @@ public class Main {
         *   b) wyświetlić dane za pomocą println i printf
         */
 
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Podaj imię: ");
+        String imie = scan.next();
+        System.out.print("Podaj nazwisko: ");
+        String nazwisko = scan.next();
+        System.out.print("Podaj numer albumu: ");
+        int numerIndexu = scan.nextInt();
+        System.out.println("Witaj " + imie + " " + nazwisko + " " + numerIndexu);
+        System.out.printf("Witaj %s %s %d \n", imie, nazwisko, numerIndexu);
+
+
 
         /*  zad.2 zadeklarować 2 zmienne typu int i wykonać na nich obliczenia arytmetyczn (+, -, *, /, %)
         *   wynik wyświetlić w konsoli programu
         */
+
+        int liczbaA, liczbaB;
+        System.out.print("Podaj pierwszą liczbę: ");
+        liczbaA = scan.nextInt();
+        System.out.print("Podaj drugą liczbę: ");
+        liczbaB = scan.nextInt();
+        System.out.printf("%d + %d = %d\n",liczbaA, liczbaB, liczbaA + liczbaB);
+        System.out.printf("%d - %d = %d\n",liczbaA, liczbaB, liczbaA - liczbaB);
+        System.out.printf("%d * %d = %d\n",liczbaA, liczbaB, liczbaA * liczbaB);
+        System.out.printf("%d / %d = %d\n",liczbaA, liczbaB, liczbaA / liczbaB);
+        System.out.printf("%d mod %d = %d\n",liczbaA, liczbaB, liczbaA % liczbaB);
+
 
     }
 }
