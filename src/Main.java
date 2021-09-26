87d504d3933bfe9bb9f18681f6b0b9e19b234474
weron <weronikawiecaszek@wp.pl>
2021-09-26 10:56:38 +0200

    ćwiczenie czwarte

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,10 @@ public class Main {
         /* ćw.3 */
 
         /*  zad.1 napisać interfejs który bedzie słuzył do wpisania danych osobowych:
-        *   a) Imię, nazwisko, wiek, nr indeksu,
-        *   dane mają być wprowadzane z klawiatury w konsoli
-        *   b) wyświetlić dane za pomocą println i printf
-        */
+         *   a) Imię, nazwisko, wiek, nr indeksu,
+         *   dane mają być wprowadzane z klawiatury w konsoli
+         *   b) wyświetlić dane za pomocą println i printf
+         */
 
         Scanner scan = new Scanner(System.in);
         System.out.print("Podaj imię: ");
@@ -27,19 +27,19 @@ public class Main {
 
 
         /*  zad.2 zadeklarować 2 zmienne typu int i wykonać na nich obliczenia arytmetyczn (+, -, *, /, %)
-        *   wynik wyświetlić w konsoli programu
-        */
+         *   wynik wyświetlić w konsoli programu
+         */
 
         int liczbaA, liczbaB;
         System.out.print("Podaj pierwszą liczbę: ");
         liczbaA = scan.nextInt();
         System.out.print("Podaj drugą liczbę: ");
         liczbaB = scan.nextInt();
-        System.out.printf("%d + %d = %d\n",liczbaA, liczbaB, liczbaA + liczbaB);
-        System.out.printf("%d - %d = %d\n",liczbaA, liczbaB, liczbaA - liczbaB);
-        System.out.printf("%d * %d = %d\n",liczbaA, liczbaB, liczbaA * liczbaB);
-        System.out.printf("%d / %d = %d\n",liczbaA, liczbaB, liczbaA / liczbaB);
-        System.out.printf("%d mod %d = %d\n",liczbaA, liczbaB, liczbaA % liczbaB);
+        System.out.printf("%d + %d = %d\n", liczbaA, liczbaB, liczbaA + liczbaB);
+        System.out.printf("%d - %d = %d\n", liczbaA, liczbaB, liczbaA - liczbaB);
+        System.out.printf("%d * %d = %d\n", liczbaA, liczbaB, liczbaA * liczbaB);
+        System.out.printf("%d / %d = %d\n", liczbaA, liczbaB, liczbaA / liczbaB);
+        System.out.printf("%d mod %d = %d\n", liczbaA, liczbaB, liczbaA % liczbaB);
 
 
     }
